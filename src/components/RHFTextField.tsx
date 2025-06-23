8f2a676f1c9d6f51a32ce6060923edf37fedb405
Sadanand Gopal <sadanand.gopal@mindteck.com>
2025-06-23 22:41:22 +0530

    Refactor RHFTextField and Login components to use FormProvider for better context management

--- src/components/RHFTextField.tsx
+++ src/components/RHFTextField.tsx
@@ -1,12 +1,12 @@
-import { Controller, Control, FieldValues, FieldPath } from 'react-hook-form';
+import { Controller, FieldValues, FieldPath, useFormContext } from 'react-hook-form';
 import { TextField, TextFieldProps } from '@mui/material';
 
-interface RHFTextFieldProps<T extends FieldValues> extends Omit<TextFieldProps, 'name'> {
+interface RHFTextFieldProps<T extends FieldValues> extends Omit<TextFieldProps, 'name' | 'control'> {
   name: FieldPath<T>;
-  control: Control<T>;
 }
 
-function RHFTextField<T extends FieldValues>({ name, control, ...props }: RHFTextFieldProps<T>) {
+function RHFTextField<T extends FieldValues>({ name, ...props }: RHFTextFieldProps<T>) {
+  const { control } = useFormContext<T>();
   return (
     <Controller
       name={name}
